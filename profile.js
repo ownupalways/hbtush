a97818c5739acf58c851ab8623aa761c54da34ba
Godwin Oluwadipe <gwinsworld@gmail.com>
2024-08-30 23:05:36 +0100

    Added Theme Operation, and modify code into style root file

--- profile.js
+++ profile.js
@@ -1,12 +1,33 @@
 let themeBlack = document.querySelector(".theme #nightLight")
 
+
+function updatedTheme(theme) {
+    document.body.className = theme
+}
+
 themeBlack.addEventListener('click', changeBackgroundToBlack)
 
 function changeBackgroundToBlack() {
-    document.body.classList.toggle("newTheme")
+    let currentTheme = localStorage.getItem('theme')
+    console.log(currentTheme)
+    let changedTheme = currentTheme === 'newTheme' ? 'MyBody' : 'newTheme'
+    updatedTheme(changedTheme)
     if(document.body.classList.contains("newTheme")) {
         themeBlack.src = "image/sun.png"
     }else {
         themeBlack.src = "image/moon.png"
     }
+    localStorage.setItem('theme', changedTheme);
 }
+
+// Apply the saved theme on page load
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    updatedTheme(savedTheme);
+
+    if(document.body.classList.contains("newTheme")) {
+        themeBlack.src = "image/sun.png"
+    }else {
+        themeBlack.src = "image/moon.png"
+    }
+});
